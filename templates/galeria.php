1e330443067a54cc05c444b4036a77cf0d641e6f
Marcos <m.cires03@gmail.com>
2024-10-16 12:20:12 +0200

    Fifth commit (imagenRepository)

--- templates/galeria.php
+++ templates/galeria.php
@@ -5,35 +5,34 @@ require_once __DIR__ . '/../src/entity/imagen.class.php';
 require_once __DIR__ . '/../src/database/connection.class.php';
 require_once __DIR__ . '/../src/database/querybuilder.class.php';
 require_once __DIR__ . '/../src/exceptions/fileexception.class.php';
+require_once __DIR__ . '/../src/repository/ImagenRepository.php';
 
 try {
     $config = require_once __DIR__ . '/../app/config.php';
     App::bind('config', $config); // Guardamos la configuración en el contenedor de servicios
     $conexion = App::getConnection();
 
+    $queryBuilder = new QueryBuilder('imagenes', 'Imagen');
+    $imagenesRepository = new ImagenesRepository();
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $descripcion = trim(htmlspecialchars($_POST['descripcion']));
         $titulo = trim(htmlspecialchars($_POST['titulo']));
         $tiposAceptados = ['image/jpeg', 'image/gif', 'image/png'];
         $imagen = new File('imagen', $tiposAceptados); // El nombre 'imagen' es el que se ha puesto en el formulario de galeria.view.php
+
         $imagen->saveUploadFile(Imagen::RUTA_IMAGENES_SUBIDAS);
-        $sql = "INSERT INTO imagenes (nombre, descripcion, categoria) VALUES
-     (:nombre,:descripcion,:categoria)";
-        $pdoStatement = $conexion->prepare($sql);
-        $parametros = [
-            ':nombre' => $imagen->getFileName(),
-            ':descripcion' => $descripcion,
-            ':categoria' => '1'
-        ];
-        if ($pdoStatement->execute($parametros) === false)
-            $errores[] = "No se ha podido guardar la imagen en la base de datos";
-        else
-            $mensaje = "Se ha guardado la imagen correctamente";
+
+        $imagenGaleria = new Imagen($imagen->getFileName(), $descripcion);
+        $imagenesRepository->save($imagenGaleria);
+
+        $mensaje = "Se ha guardado la imagen correctamente";
+        $imagenes = $imagenesRepository->findAll();
     } else {
         $descripcion = "";
         $titulo = "";
     }
-    $queryBuilder = new QueryBuilder('imagenes','Imagen');
+    $queryBuilder = new QueryBuilder('imagenes', 'Imagen');
     $imagenes = $queryBuilder->findAll();
 } catch (FileException $fileException) {
     $errores[] = $fileException->getMessage();
